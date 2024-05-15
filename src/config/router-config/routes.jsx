1321b638a6f0884f0e06087a3274c39a0f1fc3e2
Ibad <ibadurrehman718@gmail.com>
2024-05-16 03:46:26 +0500

    navbar done

--- src/config/router-config/routes.jsx
+++ src/config/router-config/routes.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../../screens/home/home'
 import About from '../../screens/about/about'
 import Contact from '../../screens/contact/contact'
+import Navbar from '../../components/Navbar'
 
 const Routers = () => {
     return (
         <>
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path='/' element={< Home />} />
-                    <Route path='/products' element={<Contact />} />
                     <Route path='about' element={<About />} />
+                    <Route path='/contact' element={<Contact />} />
                 </Routes>
 
             </BrowserRouter>
