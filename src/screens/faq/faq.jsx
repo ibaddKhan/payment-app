a6e834e6118e86ec08d523af21e219b871fab93a
ZiadurRehman718 <ziadshah718@gmail.com>
2024-05-17 15:59:21 +0500

    FAQ page added

--- src/screens/faq/faq.jsx
+++ src/screens/faq/faq.jsx
@@ -1,8 +1,60 @@
-
 const Faq = () => {
-    return (
-        <div>Faq</div>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-5xl cursor-default font-semibold my-10">Frequently Asked Questions</h1>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-8/12">
+          <details
+            open
+            tabIndex="0"
+            className="collapse collapse-arrow my-2 bg-base-200"
+          >
+            <summary className="collapse-title text-xl font-medium">
+              Click to open/close
+            </summary>
+            <div className="collapse-content">
+              <p>content</p>
+            </div>
+          </details>
 
-export default Faq
+          <details open className="collapse collapse-arrow my-2 bg-base-200">
+            <summary className="collapse-title text-xl font-medium">
+              Click to open/close
+            </summary>
+            <div className="collapse-content">
+              <p>content</p>
+            </div>
+          </details>
+          <details open className="collapse collapse-arrow my-2 bg-base-200">
+            <summary className="collapse-title text-xl font-medium">
+              Click to open/close
+            </summary>
+            <div className="collapse-content">
+              <p>content</p>
+            </div>
+          </details>
+          <details open className="collapse collapse-arrow my-2 bg-base-200">
+            <summary className="collapse-title text-xl font-medium">
+              Click to open/close
+            </summary>
+            <div className="collapse-content">
+              <p>content</p>
+            </div>
+          </details>
+          <details open className="collapse collapse-arrow my-2 bg-base-200">
+            <summary className="collapse-title text-xl font-medium">
+              Click to open/close
+            </summary>
+            <div className="collapse-content">
+              <p>content</p>
+            </div>
+          </details>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Faq;
