c377e950018b38d0adf432fc007211bae6d5e756
Ibad <ibadurrehman718@gmail.com>
2024-05-19 07:31:26 +0500

    checkout left all done

--- src/screens/Terms/terms.jsx
+++ src/screens/Terms/terms.jsx
@@ -3,41 +3,6 @@
 const Terms = () => {
   return (
     <div className="container md:w-4/5  mx-auto shadow-2xl my-8">
-      <div className="max-w-3/4 mx-auto">
-        <h1 className="text-center text-5xl font-semibold my-10">Terms and Conditions</h1>
-
-        <div className="text-lg">
-          <p className="p-4">These Terms and Conditions ("Terms") govern your use of the pay boost bills website, products, and services (collectively referred to as "Services"). By accessing or using our Services, you agree to comply with and be bound by these Terms. If you do not agree to these Terms, please do not use our Services.</p>
-
-          <h2 className="text-2xl p-4">1. Acceptance of Terms</h2>
-          <p className="p-4">By using our Services, you confirm that you have read, understood, and agree to these Terms, and you represent and warrant that you have the legal capacity to enter into this agreement.</p>
-
-          <h2 className="text-2xl p-4">2. Changes to Terms</h2>
-          <p className="p-4">pay boost bills reserves the right to modify, revise, or update these Terms at any time. Any changes will be effective immediately upon posting on our website. Your continued use of the Services after any changes constitute your acceptance of the updated Terms.</p>
-
-          <h2 className="text-2xl p-4">3. Use of Services</h2>
-          <div className="p-4">
-            <p>(a) Eligibility: You must be at least 16 years old to use our Services.</p>
-            <p>(b) Account Registration: Some Services may require you to create an account. You agree to provide accurate, current, and complete information during the registration process and to update such information to keep it accurate, current, and complete.</p>
-            <p>(c) User Conduct:</p>
-            <ul>
-              <li>Violating any applicable laws or regulations.</li>
-              <li>Impersonating any person or entity.</li>
-              <li>Uploading, posting, transmitting, or distributing any content that is harmful, illegal, defamatory, obscene, or infringing upon the rights of others.</li>
-              <li>Attempting to gain unauthorized access to our systems or user accounts.</li>
-            </ul>
-          </div>
-
-          <h2 className="text-2xl p-4">4. Intellectual Property</h2>
-          <div className="p-4">
-            <p>(a) Ownership: All content, trademarks, logos, and intellectual property associated with pay boost bills are owned by pay boost bills or its licensors and are protected by copyright and other intellectual property laws.</p>
-            <p>(b) Limited License: You are granted a limited, non-exclusive, non-transferable license to access and use our Services for personal, non-commercial purposes. This license does not grant you any ownership rights.</p>
-          </div>
-
-          <h2 className="text-2xl p-4">5. Privacy</h2>
-          <p className="p-4">Your use of our Services is subject to our Privacy Policy, which outlines how we collect, use, and protect your personal information. By using our Services, you consent to the practices described in the Privacy Policy.</p>
-        </div>
-      </div>
     </div>
   );
 }
--- src/screens/about/about.jsx
+++ src/screens/about/about.jsx
@@ -8,15 +8,15 @@ const About = () => {
             </div>
             <div className="w-full lg:w-1/2 flex flex-col shadow-2xl items-center justify-center">
                 <div className="p-6 rounded-lg shadow-md ">
-                    <h2 className="text-3xl lg:text-4xl font-semibold mb-4 text-center">About Us Content</h2>
+                    <h2 className="text-3xl lg:text-4xl font-semibold mb-4 text-center">About Us</h2>
                     <p className="text-lg mb-4">
-                    Welcome to Pay Boost Bills, your trusted partner for simple and effective payment solutions. At Pay Boost Bills, we understand the hassle and stress that can come with managing multiple monthly payments. That’s why we’re dedicated to creating an easy-to-use platform that simplifies the entire process, and gives you more time to focus on what really matters
+                        Welcome to Pay Boost Bills, your trusted partner for simple and effective payment solutions. At Pay Boost Bills, we understand the hassle and stress that can come with managing multiple monthly payments. That’s why we’re dedicated to creating an easy-to-use platform that simplifies the entire process, and gives you more time to focus on what really matters
                     </p>
                     <p className="text-lg mb-4">
-                    Our mission is simple: to pay bills quickly, securely and hassle-free. Whether you’re paying utility bills, phone bills, or other recurring expenses, Pay Boost Bills offers the perfect solution to keep your finances running. Our platform is built with the latest technology to ensure every transaction is smooth and secure.
+                        Our mission is simple: to pay bills quickly, securely and hassle-free. Whether you’re paying utility bills, phone bills, or other recurring expenses, Pay Boost Bills offers the perfect solution to keep your finances running. Our platform is built with the latest technology to ensure every transaction is smooth and secure.
                     </p>
                     <p className="text-lg mb-4">
-                    With Pay Boost Bills, you can manage all your payments in one place. Our user-friendly interface lets you set up automatic payments, track your payment history, and receive timely reminders to ensure you never miss a due date Moreover, by way paying for your Boost Mobile via PayPal, we make it easy to customize to suit your lifestyle and we offer.
+                        With Pay Boost Bills, you can manage all your payments in one place. Our user-friendly interface lets you set up automatic payments, track your payment history, and receive timely reminders to ensure you never miss a due date Moreover, by way paying for your Boost Mobile via PayPal, we make it easy to customize to suit your lifestyle and we offer.
                     </p>
                     <p className="text-lg mb-4">We pride ourselves on exceptional customer service and are committed to providing assistance whenever you need it. Our team of experts are always ready to help with any questions or concerns you may have, ensuring that your experience with Pay Boost Bills is nothing but positive.
                     </p>
